c1c65b910adacf6f7cc178fec07e68eb854fcb52
rudra-192005 <rudrashandilya07@icloud.com>
2026-03-02 14:15:20 +0530

    Add main block to run app and create database

---
+++ app.py
@@ -0,0 +1,5 @@
+# At the bottom of app.py, make sure this block exists:
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True, host='0.0.0.0', port=5000)
